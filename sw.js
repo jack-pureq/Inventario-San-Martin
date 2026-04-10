1407d870e1847e6ef56fa1f6ef4cbc47398b1041
jack-pureq <mvob.51015@gmail.com>
2026-04-10 10:39:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
-const CACHE_NAME = 'taller-v1';
+const CACHE_NAME = 'taller-v2'; // Cambiamos el nombre de v1 a v2
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// Instalar el Service Worker
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Esto obliga al nuevo Service Worker a activarse de inmediato
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Hacer que la app funcione offline
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
